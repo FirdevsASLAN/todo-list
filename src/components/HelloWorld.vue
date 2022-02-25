7e3f885a53ff5556de7c794a5434b9f84dceddc7
Firdevs Aslan <firdevsaslan6@gmail.com>
2022-02-25 18:51:29 +0300

    add checkbox and isComplated property on todItem

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,10 +6,27 @@
         <v-text-field label="Todo Input" v-model="todoInput" outlined />
         <v-btn elevation="2" @click="addTodo">Add Todo</v-btn>
         <div v-if="todoList.length > 0">
-          <div v-for="(todoItem, index) in todoList" v-bind:key="index">
-            {{ todoItem }}
-            <v-icon @click="editTodo(todoItem)">mdi-pencil-outline </v-icon>
-            <v-icon @click="deleteTodo(todoItem)">mdi-delete</v-icon>
+          <div
+            v-for="(todoItem, index) in todoList"
+            v-bind:key="index"
+            class="main__container d-flex align-center justify-space-between mx-auto"
+          >
+            <div class="d-flex">
+              <v-simple-checkbox v-model="todoItem.isComplated" class="mr-3" />
+              <label
+                :class="{
+                  'text-decoration-line-through': todoItem.isComplated,
+                }"
+              >
+                {{ todoItem.todo }}
+              </label>
+            </div>
+            <div>
+              <v-icon @click="editTodo(todoItem.todo)"
+                >mdi-pencil-outline
+              </v-icon>
+              <v-icon @click="deleteTodo(todoItem.todo)">mdi-delete</v-icon>
+            </div>
           </div>
         </div>
       </v-col>
@@ -29,7 +46,11 @@ export default {
   methods: {
     addTodo() {
       if (this.todoInput.length > 0) {
-        this.todoList.push(this.todoInput);
+        this.todoList.push({
+          todo: this.todoInput,
+          isComplated: false,
+          isEdit: false,
+        });
       }
       this.todoInput = "";
       console.log(this.todoList);
@@ -48,6 +69,9 @@ export default {
 .main {
   &__title {
     margin-top: 10rem;
+  }
+  &__container {
+    width: 60%;
   }
 }
 </style>
